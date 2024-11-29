fb6e71d552c71bbacbf62e2502aadb333d76dde5
lguerra <l.guerra--rago@xefi;fr>
2024-11-29 13:54:39 +0100

    wip

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,8 @@
-import {addDynamicIconSelectors} from "@iconify/tailwind";
-
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {},
 	},
-	plugins: [
-		addDynamicIconSelectors(),
-	],
-}
+	plugins: [],
+};
