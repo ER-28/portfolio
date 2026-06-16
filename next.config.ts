0f081314a32be47835aa05f6d1092e1f636c498b
ER28 <lylian.gr@protonmail.com>
2026-06-16 11:33:27 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/portfolio",
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-    path: "/portfolio"
-  },
 };
 
 export default nextConfig;
