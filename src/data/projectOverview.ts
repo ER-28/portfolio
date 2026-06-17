e169649ec8f9e5f12db654040ad3eb9c27ce689b
ER28 <lylian.gr@protonmail.com>
2026-06-17 10:05:51 +0200

    Update projectOverview.ts

--- src/data/projectOverview.ts
+++ src/data/projectOverview.ts
@@ -4,6 +4,7 @@ export const projectOverview = {
     ],
     sideProjects: [
         {name: "Nestjs Response Wrapper", href: "https://er-28.github.io/nestjs-response-wrapper/"},
+        {name: "Photo Portfolio", href: "https://er-28.github.io/photofolio/"},
         {name: "Reysin", comingSoon: true},
         {name: "LumoraPlan", comingSoon: true},
     ]
